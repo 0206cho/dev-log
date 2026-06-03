54059a795b01b5229b9f31cba589ae645c7e1574
MinSol <68586044+0206cho@users.noreply.github.com>
2026-06-03 13:53:14 +0900

    [Passed] 증가시키며 출력하기 (104ms, 9MB)

---
+++ Algorithm/codetree/trail1/증가시키며 출력하기/increase-and-print.java
@@ -0,0 +1,10 @@
+// 5부터 17까지 2씩 증가하면서 출력하는 프로그램
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i=5; i<=17; i+=2){
+            System.out.print(i + " ");
+        }
+        
+    }
+}
