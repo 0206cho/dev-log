ddbf3cdb9cf7d635ed9ab845aee758fae812e265
MinSol <68586044+0206cho@users.noreply.github.com>
2026-06-04 16:26:10 +0900

    [Passed] 증가시키며 출력하기 2 (99ms, 9MB)

---
+++ Algorithm/codetree/trail1/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,11 @@
+// while문을 사용해 10부터 26까지 1씩 증가하며 출력하는 프로그램
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        // 출력
+        while(i<=26){
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
