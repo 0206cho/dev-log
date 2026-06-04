2d922916cb3f70fd83b9ab5a13fd64ee25823505
MinSol <68586044+0206cho@users.noreply.github.com>
2026-06-05 08:15:53 +0900

    [Passed] 감소시키며 출력하기 2 (88ms, 9MB)

---
+++ Algorithm/codetree/trail1/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,10 @@
+// while문을 사용해 26부터 10까지 1씩 감소하며 출력하는 프로그램
+public class Main {
+    public static void main(String[] args) {
+        int i = 26;
+        while(i>=10) {
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
