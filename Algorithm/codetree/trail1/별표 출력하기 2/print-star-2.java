ae6b634b133f0ee93811f43fed70bf0d0662710e
MinSol <68586044+0206cho@users.noreply.github.com>
2026-06-18 11:13:52 +0900

    [Passed] 별표 출력하기 2 (137ms, 10MB)

---
+++ Algorithm/codetree/trail1/별표 출력하기 2/print-star-2.java
@@ -0,0 +1,22 @@
+/*  
+    정수 N의 값을 입력받아 별표를 출력하는 프로그램
+    * * *
+    * *
+    *
+*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int j=n; j>i; j--){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+        sc.close();
+    }
+}
